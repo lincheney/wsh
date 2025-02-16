4f0e9d7ea99701a496a28f91af52b0bd998532f5
林千里 <lincheney@gmail.com>
2025-02-17 00:22:24 +1100

    history file

---
+++ src/history.rs
@@ -0,0 +1,73 @@
+use std::collections::HashMap;
+use anyhow::Result;
+use mlua::{prelude::*};
+use crate::ui::Ui;
+use crate::shell::Shell;
+use bstr::*;
+
+async fn get_history(_ui: Ui, shell: Shell, _lua: Lua, _val: ()) -> Result<(HashMap<usize, BString>, usize)> {
+    let mut shell = shell.lock().await;
+    let history: HashMap<_, _> = shell.get_history().entries().map(|e| (e.histnum as _, e.text)).collect();
+    let curhist = shell.get_curhist().0;
+    Ok((history, curhist as _))
+}
+
+async fn get_history_index(_ui: Ui, shell: Shell, _lua: Lua, _val: ()) -> Result<usize> {
+    Ok(shell.lock().await.get_curhist().0 as _)
+}
+
+async fn set_history_index(_ui: Ui, shell: Shell, _lua: Lua, val: usize) -> Result<(usize, Option<BString>)> {
+    let mut shell = shell.lock().await;
+    let (curhist, entry) = shell.set_curhist(val as _);
+    Ok((curhist as _, entry.map(|e| e.as_entry().unwrap().text)))
+}
+
+async fn get_next_history(_ui: Ui, shell: Shell, _lua: Lua, val: usize) -> Result<(Option<usize>, Option<BString>)> {
+    let mut shell = shell.lock().await;
+    // get the next highest one
+    let value = shell
+        .get_history()
+        .enumerate()
+        .take_while(|(h, _)| *h > val as _)
+        .map(|(h, e)| (h as _, e.as_entry().unwrap().text))
+        .last()
+    ;
+    Ok(value.unzip())
+}
+
+async fn get_prev_history(_ui: Ui, shell: Shell, _lua: Lua, val: usize) -> Result<(Option<usize>, Option<BString>)> {
+    let mut shell = shell.lock().await;
+    // get the next lowest one
+    let value = shell
+        .get_history()
+        .enumerate()
+        .find(|(h, _)| *h < val as _)
+        .map(|(h, e)| (h as _, e.as_entry().unwrap().text))
+    ;
+    Ok(value.unzip())
+}
+
+async fn goto_history(ui: Ui, shell: Shell, lua: Lua, val: usize) -> Result<(usize, Option<BString>)> {
+    let (curhist, text) = set_history_index(ui.clone(), shell.clone(), lua, val).await?;
+    ui.borrow_mut().await.buffer.mutate(|buffer, cursor, _| {
+        buffer.clear();
+        if let Some(text) = &text {
+            buffer.clone_from_slice(text.as_bytes());
+        }
+        *cursor = buffer.len();
+    });
+    Ok((curhist, text))
+}
+
+pub async fn init_lua(ui: &Ui, shell: &Shell) -> Result<()> {
+
+    ui.set_lua_async_fn("get_history", shell, get_history).await?;
+    ui.set_lua_async_fn("get_history_index", shell, get_history_index).await?;
+    ui.set_lua_async_fn("set_history_index", shell, set_history_index).await?;
+    ui.set_lua_async_fn("get_next_history", shell, get_next_history).await?;
+    ui.set_lua_async_fn("get_prev_history", shell, get_prev_history).await?;
+    ui.set_lua_async_fn("goto_history", shell, goto_history).await?;
+
+    Ok(())
+}
+
