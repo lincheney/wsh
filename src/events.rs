bc5415e778123a1023f2a5764ebdd073e5d67e23
林千里 <lincheney@gmail.com>
2025-02-24 22:49:03 +1100

    remove callbacks

--- src/events.rs
+++ src/events.rs
@@ -6,21 +6,24 @@ use crate::shell::Shell;
 use crossterm::event;
 
 macro_rules! event_types {
-    ($( $name:ident($($arg:ty),*) ),*) => (
+    ($( $name:ident($($arg:ty),*), )*) => (
 
         #[derive(Debug, Deserialize, Clone, Copy)]
         pub enum EventType {
+        $(
             #[allow(non_camel_case_types)]
-            $($name),*
+            $name,
+        )*
         }
 
     paste::paste!{
 
         #[derive(Default)]
         pub struct EventCallbacks {
+            counter: usize,
         $(
-            [<callbacks_ $name>]: Vec<Function>,
-        ),*
+            [<callbacks_ $name>]: Vec<(usize, Function)>,
+        )*
         }
 
         impl EventCallbacks {
@@ -28,7 +31,7 @@ macro_rules! event_types {
             #[allow(unused_parens)]
             pub fn [<trigger_ $name _callbacks>](&self, ui: &Ui, shell: &Shell, lua: &Lua, val: ($($arg),*)) {
                 let val = lua.to_value(&val).unwrap();
-                for cb in self.[<callbacks_ $name>].iter() {
+                for (_, cb) in self.[<callbacks_ $name>].iter() {
                     ui.call_lua_fn(shell.clone(), false, cb.clone(), val.clone());
                 }
             }
@@ -36,24 +39,37 @@ macro_rules! event_types {
             pub fn [<has_ $name _callbacks>](&self) -> bool {
                 !self.[<callbacks_ $name>].is_empty()
             }
-        ),*
+        )*
 
-            fn get_callbacks(&self, typ: EventType) -> &Vec<Function> {
+            fn get_callbacks(&self, typ: EventType) -> &Vec<(usize, Function)> {
                 match typ {
                 $( EventType::$name => &self.[<callbacks_ $name>], )*
                 }
             }
 
-            fn get_callbacks_mut(&mut self, typ: EventType) -> &mut Vec<Function> {
+            fn get_callbacks_mut(&mut self, typ: EventType) -> &mut Vec<(usize, Function)> {
                 match typ {
                 $( EventType::$name => &mut self.[<callbacks_ $name>], )*
                 }
             }
 
-
-            fn add_event_callback(&mut self, typ: EventType, cb: Function) {
-                self.get_callbacks_mut(typ).push(cb);
+            fn add_event_callback(&mut self, typ: EventType, cb: Function) -> usize {
+                let counter = self.counter;
+                self.get_callbacks_mut(typ).push((counter, cb));
+                self.counter = self.counter + 1;
+                counter
             }
+
+            fn remove_event_callback(&mut self, id: usize) {
+            $(
+                let callbacks = &mut self.[<callbacks_ $name>];
+                if let Some(i) = callbacks.iter().position(|(i, _)| *i == id) {
+                    callbacks.remove(i);
+                    return
+                }
+            )*
+            }
+
         }
     }
 
@@ -80,18 +96,25 @@ impl From<event::KeyEvent> for KeyEvent {
     }
 }
 
-event_types!(key(KeyEvent));
+event_types!(
+    key(KeyEvent),
+);
 
 
-async fn add_event_callback(mut ui: Ui, _shell: Shell, lua: Lua, (typ, callback): (LuaValue, Function)) -> Result<()> {
+async fn add_event_callback(mut ui: Ui, _shell: Shell, lua: Lua, (typ, callback): (LuaValue, Function)) -> Result<usize> {
     let typ: EventType = lua.from_value(typ)?;
-    ui.borrow_mut().await.event_callbacks.add_event_callback(typ, callback);
+    Ok(ui.borrow_mut().await.event_callbacks.add_event_callback(typ, callback))
+}
+
+async fn remove_event_callback(mut ui: Ui, _shell: Shell, _lua: Lua, id: usize) -> Result<()> {
+    ui.borrow_mut().await.event_callbacks.remove_event_callback(id);
     Ok(())
 }
 
 pub async fn init_lua(ui: &Ui, shell: &Shell) -> Result<()> {
 
     ui.set_lua_async_fn("add_event_callback", shell, add_event_callback).await?;
+    ui.set_lua_async_fn("remove_event_callback", shell, remove_event_callback).await?;
 
     Ok(())
 }
