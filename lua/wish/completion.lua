7d8a717b302bdfab8d5462a2454518047cf0c1e0
林千里 <lincheney@gmail.com>
2025-02-28 22:16:31 +1100

    handle no matches

--- lua/wish/completion.lua
+++ lua/wish/completion.lua
@@ -52,31 +52,38 @@ function M.complete()
 end
 
 function M.complete()
-    -- go to last line
     local cursor = wish.get_cursor()
-    wish.set_cursor(wish.str.len(wish.get_buffer()))
-    wish.redraw()
-    -- then down one
-    io.stdout:write('\r\n')
-    io.stdout:flush()
 
     -- start fzf
-    local proc = wish.async.spawn{
-        args={
-            'fzf',
-            '--height=40%',
-            '--reverse',
-            '--with-nth=2..',
-        },
-        foreground=true,
-        stdin='piped',
-        stdout='piped',
-    }
+    local function start_fzf()
+        -- go to last line
+        wish.set_cursor(wish.str.len(wish.get_buffer()))
+        wish.redraw()
+        -- then down one
+        io.stdout:write('\r\n')
+        io.stdout:flush()
+
+        return wish.async.spawn{
+            args={
+                'fzf',
+                '--exit-0',
+                '--height=40%',
+                '--reverse',
+                '--with-nth=2..',
+            },
+            foreground=true,
+            stdin='piped',
+            stdout='piped',
+        }
+    end
+
+    local proc = nil
     -- get completions and feed to fzf
     local comp = wish.get_completions()
     local matches = {}
     local ok, err = pcall(function()
         for chunk in comp do
+
             local str = {}
             for i = 1, #chunk do
                 if tostring(chunk[i]) then
@@ -84,28 +91,43 @@ function M.complete()
                     table.insert(str, string.format('%i\t%s\n', #matches, chunk[i]))
                 end
             end
-            proc.stdin:write(table.concat(str, ''))
+
+            if #str then
+                -- start fzf
+                proc = proc or start_fzf()
+                proc.stdin:write(table.concat(str, ''))
+            end
         end
     end)
-    proc.stdin:close()
-    local code = proc:wait()
 
-    local num = tonumber(proc.stdout:read_all():match('^(%d+)\t'))
+    if proc then
+        proc.stdin:close()
+        local code = proc:wait()
+        local num = tonumber(proc.stdout:read_all():match('^(%d+)\t'))
+        -- go back up
+        io.stdout:write('\x1b[A')
+        io.stdout:flush()
 
-    -- go back up
-    io.stdout:write('\x1b[A')
-    io.stdout:flush()
-
-    wish.redraw()
-    wish.set_cursor(cursor)
-    wish.redraw()
-
-    if not ok then
-        error(err)
-    elseif code == 0 and matches[num] then
-        wish.insert_completion(matches[num])
+        wish.redraw{all=true}
+        wish.set_cursor(cursor)
         wish.redraw()
+
+        if not ok then
+            error(err)
+        elseif code == 0 and matches[num] then
+            wish.insert_completion(matches[num])
+            wish.redraw()
+        end
+
+    else
+        if not ok then
+            error(err)
+        end
+        wish.show_message{text='No completion matches', fg='lightred'}
+        wish.redraw()
+
     end
+
 
 end
 
