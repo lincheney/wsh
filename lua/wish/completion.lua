d1f2b79a52ddefa0e62558e1075c0dc761493473
林千里 <lincheney@gmail.com>
2025-03-29 00:52:29 +1100

    loading spinner for completion

--- lua/wish/completion.lua
+++ lua/wish/completion.lua
@@ -4,6 +4,19 @@ local SELECTION = require('wish/selection-widget')
 function M.complete()
     local matches = nil
     local comp = wish.get_completions()
+    local loaded = false
+    local msg = wish.set_message{hidden = true}
+
+    -- loading spinner thing
+    wish.schedule(function()
+        local dots = 1
+        while not loaded do
+            wish.set_message{id = msg, hidden = false, text = 'Loading matches ' .. string.rep('.', dots)}
+            wish.redraw()
+            dots = (dots + 1) % 4
+            wish.async.sleep(200)
+        end
+    end)
 
     local result = SELECTION.start{
         source = function()
@@ -11,9 +24,11 @@ function M.complete()
             return function()
                 while true do
                     local chunk = comp()
+                    loaded = true
                     if not chunk then
                         return
                     end
+                    wish.set_message{id = msg, hidden = true}
 
                     local filtered_chunk = {}
                     for i = 1, #chunk do
@@ -37,7 +52,7 @@ function M.complete()
         wish.insert_completion(matches[result])
         wish.redraw()
     elseif #matches == 0 then
-        wish.set_message{text='No completion matches', fg='lightred'}
+        wish.set_message{id = msg, hidden = false, text='No completion matches', fg='lightred'}
         wish.redraw()
     end
 
