e1222dfe946ec900f825179f2de3a59aad3a6422
林千里 <lincheney@gmail.com>
2025-12-14 23:28:30 +1100

    split syntax parsing into separate module

--- lua/wish/syntax-highlight.lua
+++ lua/wish/syntax-highlight.lua
@@ -1,19 +1,5 @@
-local prev_buffer = nil
-local prev_complete = false
-
+local QUERY = require('wish/syntax-query')
 local NAMESPACE = wish.add_buf_highlight_namespace()
-
-local function debug_tokens(tokens, buffer)
-    local x = {}
-    for i = 1, #tokens do
-        x[i] = {
-            buffer:sub(tokens[i].start+1, tokens[i].finish),
-            tokens[i].kind,
-            tokens[i].nested and debug_tokens(tokens[i].nested, buffer)
-        }
-    end
-    return x
-end
 
 local RULES = {
     { { hl='command', kind='command' } },
@@ -88,221 +74,72 @@ local RULES = {
     -- { { hl='error', regex='^\\{$' }, { not_regex='\\}', mod='*' }, { mod='$' } },
 }
 
-local apply_highlight_seq
-
-local function apply_highlight_matcher(matcher, token, str)
-
-    if matcher.contains and not token.nested then
-        -- matcher asserts nested tokens but there aren't any
+local function apply_highlight_matcher(matcher, token, str, highlights, priority)
+    if not matcher.hl then
         return
     end
 
-    local tokstr = nil
-    local kind = token.kind or ''
-    if matcher.kind then
-        if type(matcher.kind) == 'string' then
-            matcher.kind = wish.regex(matcher.kind)
+    if matcher.hlregex then
+        if type(matcher.hlregex) == 'string' then
+            matcher.hlregex = wish.regex(matcher.hlregex)
         end
-        if not matcher.kind:is_full_match(kind) then
-            return
-        end
-    end
-    if matcher.not_kind then
-        if type(matcher.not_kind) == 'string' then
-            matcher.not_kind = wish.regex(matcher.not_kind)
-        end
-        if matcher.not_kind:is_full_match(kind) then
-            return
-        end
-    end
 
-    if matcher.regex or matcher.not_regex then
-        tokstr = tokstr or string.sub(str, token.start+1, token.finish)
-        if type(matcher.regex) == 'string' then
-            matcher.regex = wish.regex(matcher.regex)
-        end
-        if type(matcher.not_regex) == 'string' then
-            matcher.not_regex = wish.regex(matcher.not_regex)
-        end
-        if matcher.regex and not matcher.regex:is_match(tokstr) then
-            return
-        end
-        if matcher.not_regex and matcher.not_regex:is_match(tokstr) then
-            return
-        end
-    end
-
-    local highlights = {}
-    if matcher.hl then
-
-        if matcher.hlregex then
-            if type(matcher.hlregex) == 'string' then
-                matcher.hlregex = wish.regex(matcher.hlregex)
-            end
-
-            tokstr = tokstr or string.sub(str, token.start+1, token.finish)
-            local captures = matcher.hlregex:captures_all(tokstr)
-            for _, capture in ipairs(captures) do
-                local index = capture[2] or capture[1]
-                local hl = wish.table.copy(wish.style[matcher.hl])
-                hl.start = token.start + index[1] - 1
-                hl.finish = token.start + index[2]
-                hl.namespace = NAMESPACE
-                table.insert(highlights, hl)
-            end
-
-        else
+        local tokstr = string.sub(str, token.start+1, token.finish)
+        local captures = matcher.hlregex:captures_all(tokstr)
+        for _, capture in ipairs(captures) do
+            local index = capture[2] or capture[1]
             local hl = wish.table.copy(wish.style[matcher.hl])
-            hl.start = token.start
-            hl.finish = token.finish
+            hl.start = token.start + index[1] - 1
+            hl.finish = token.start + index[2]
             hl.namespace = NAMESPACE
+            hl.priority = priority
             table.insert(highlights, hl)
         end
+
+    else
+        local hl = wish.table.copy(wish.style[matcher.hl])
+        hl.start = token.start
+        hl.finish = token.finish
+        hl.namespace = NAMESPACE
+        hl.priority = priority
+        table.insert(highlights, hl)
     end
-
-    if matcher.contains then
-        local hl = apply_highlight_seq(matcher.contains, token.nested, str)
-        if not hl then
-            -- nested rules don't match
-            return
-        end
-        wish.table.extend(highlights, hl)
-    end
-
-    return highlights
-end
-
-local function apply_highlight_seq_at(seq, seq_index, tokens, str, token_index)
-    -- try to apply the seq[seq_index:] at tokens[token_index:] and return the end index
-    local highlights = {}
-    local non_greedy = {}
-    local matcher = seq[seq_index]
-    local mod = matcher and matcher.mod
-
-    while seq_index <= #seq do
-        if mod == '*' or mod == '*?' or mod == '?' or mod == '??' then
-            -- try the next matcher, non greedy
-            local hl, index = apply_highlight_seq_at(seq, seq_index+1, tokens, str, token_index)
-            if hl then
-                if mod == '*?' or mod == '??' then
-                    wish.table.extend(highlights, hl)
-                    -- we wanted non greedy, so return it now
-                    return highlights, index
-                end
-
-                hl = wish.iter(highlights):chain(hl):collect()
-                -- non greedy match when we wanted greedy, save for later in case the greedy match doesn't work
-                non_greedy = {hl, index}
-            end
-        end
-
-        local token = tokens[token_index]
-        local next_matcher = false
-        local hl = nil
-        if mod == '$' then
-            if token then
-                -- expected the end
-                return unpack(non_greedy)
-            end
-            next_matcher = true
-
-        elseif mod == '^' then
-            if token_index ~= 1 then
-                -- expected the start
-                return unpack(non_greedy)
-            end
-            next_matcher = true
-
-        elseif not token then
-            -- ran out of tokens before the end of the seq
-            return unpack(non_greedy)
-        else
-
-            hl = apply_highlight_matcher(matcher, token, str)
-            if mod == '*' or mod == '*?' then
-                next_matcher = not hl
-            elseif mod == '+' or mod == '+?' then
-                mod = '*' .. string.sub(mod, 2)
-                next_matcher = not hl
-            elseif hl then
-                next_matcher = true
-            else
-                -- no match
-                return unpack(non_greedy)
-            end
-        end
-
-        if hl then
-            wish.table.extend(highlights, hl)
-            token_index = token_index + 1
-        end
-
-        if next_matcher then
-            seq_index = seq_index + 1
-            matcher = seq[seq_index]
-            mod = matcher and matcher.mod
-        end
-    end
-    return highlights, token_index
-end
-
-function apply_highlight_seq(seq, tokens, str)
-    local highlights = nil
-    local token_index = 1
-    for i = 1, #tokens do
-        if i == token_index then
-            local hl, finish = apply_highlight_seq_at(seq, 1, tokens, str, token_index)
-            if hl and #hl > 0 then
-                highlights = wish.table.extend(highlights or {}, hl)
-                token_index = finish
-            else
-                token_index = token_index + 1
-            end
-        end
-    end
-    return highlights
 end
 
 local function apply_highlight_rules(rules, tokens, str, highlights, priority)
-    for i = 1, #rules do
-        local hl = apply_highlight_seq(rules[i], tokens, str)
-        if hl then
-            local p = priority + (rules[i].priority or 0) + i / #rules / 2
-            for j = 1, #hl do
-                hl[j].priority = p
-            end
-            wish.table.extend(highlights, hl)
+    priority = priority or 0
+    highlights = highlights or {}
+    local rule_priority
+    local function callback(matches)
+        for i = 1, #matches do
+            apply_highlight_matcher(matches[i][1], matches[i][2], str, highlights, rule_priority)
         end
     end
 
+    for i = 1, #rules do
+        rule_priority = (priority + (rules[i].priority or 0)) * #rules * 2 + i
+        QUERY.apply_seq(rules[i], tokens, str, callback)
+    end
     for i = 1, #tokens do
         if tokens[i].nested then
             apply_highlight_rules(rules, tokens[i].nested, str, highlights, priority+1)
         end
     end
-
     return highlights
 end
 
-wish.add_event_callback('buffer_change', function()
-    local buffer = wish.get_buffer()
-    -- rehighlight if last buffer was not a valid zsh command
-    -- or the new buffer has changed (excepting ending whitespace changes)
-    if not prev_complete or string.sub(buffer, 1, #prev_buffer) ~= prev_buffer or string.find(buffer, '%S', #prev_buffer+1) then
-        -- is this going to be slow? do we need a debounce or something?
-        local complete, tokens = wish.parse(buffer)
-        wish.log.debug(wish.repr(debug_tokens(tokens, buffer), true))
-        prev_buffer = buffer
-        prev_complete = complete
+QUERY.add_buffer_callback(function(tokens, str)
+    wish.clear_buf_highlights(NAMESPACE)
 
-        wish.clear_buf_highlights(NAMESPACE)
-
-        local highlights = apply_highlight_rules(RULES, tokens, buffer, {}, 0)
-        wish.table.sort_by(highlights, 'priority')
-        for i = 1, #highlights do
-            wish.add_buf_highlight(highlights[i])
-        end
-
-        wish.redraw{buffer = true}
+    local highlights = apply_highlight_rules(RULES, tokens, str)
+    for i = 1, #highlights do
+        highlights[i].priority = highlights[i].priority + i / #highlights / 2
     end
+    wish.table.sort_by(highlights, 'priority')
+    wish.log.debug(wish.repr(highlights, true))
+    for i = 1, #highlights do
+        wish.add_buf_highlight(highlights[i])
+    end
+
+    wish.redraw{buffer = true}
 end)
---
+++ lua/wish/syntax-query.lua
@@ -0,0 +1,217 @@
+local M = {}
+
+function M.debug_tokens(tokens, buffer)
+    local x = {}
+    for i = 1, #tokens do
+        x[i] = {
+            buffer:sub(tokens[i].start+1, tokens[i].finish),
+            tokens[i].kind,
+            tokens[i].nested and M.debug_tokens(tokens[i].nested, buffer)
+        }
+    end
+    return x
+end
+
+local function apply_matcher(matcher, token, str)
+
+    if matcher.contains and not token.nested then
+        -- matcher asserts nested tokens but there aren't any
+        return
+    end
+
+    local tokstr = nil
+    local kind = token.kind or ''
+    if matcher.kind then
+        if type(matcher.kind) == 'string' then
+            matcher.kind = wish.regex(matcher.kind)
+        end
+        if not matcher.kind:is_full_match(kind) then
+            return
+        end
+    end
+    if matcher.not_kind then
+        if type(matcher.not_kind) == 'string' then
+            matcher.not_kind = wish.regex(matcher.not_kind)
+        end
+        if matcher.not_kind:is_full_match(kind) then
+            return
+        end
+    end
+
+    if matcher.regex or matcher.not_regex then
+        tokstr = tokstr or string.sub(str, token.start+1, token.finish)
+        if type(matcher.regex) == 'string' then
+            matcher.regex = wish.regex(matcher.regex)
+        end
+        if type(matcher.not_regex) == 'string' then
+            matcher.not_regex = wish.regex(matcher.not_regex)
+        end
+        if matcher.regex and not matcher.regex:is_match(tokstr) then
+            return
+        end
+        if matcher.not_regex and matcher.not_regex:is_match(tokstr) then
+            return
+        end
+    end
+
+    local values = {{matcher, token}}
+    if matcher.contains then
+        local matched = M.apply_seq(matcher.contains, token.nested, str, function(matches)
+            wish.table.extend(values, matches)
+        end)
+        if not matched then
+            return
+        end
+    end
+
+    return values
+end
+
+local function apply_seq_at(seq, seq_index, tokens, str, token_index)
+    -- try to apply the seq[seq_index:] at tokens[token_index:] and return the end index
+    local values = {}
+    local non_greedy = {}
+    local matcher = seq[seq_index]
+    local mod = matcher and matcher.mod
+
+    while seq_index <= #seq do
+        if mod == '*' or mod == '*?' or mod == '?' or mod == '??' then
+            -- try the next matcher, non greedy
+            local index, non_greedy_values = apply_seq_at(seq, seq_index+1, tokens, str, token_index)
+
+            if index then
+                if mod == '*?' or mod == '??' then
+                    -- we wanted non greedy, so return it now
+                    return index, non_greedy_values
+                end
+
+                non_greedy_values = wish.iter(values):chain(non_greedy_values):collect()
+                -- non greedy match when we wanted greedy, save for later in case the greedy match doesn't work
+                non_greedy = {index, non_greedy_values}
+            end
+        end
+
+        local token = tokens[token_index]
+        local next_matcher = false
+        if mod == '$' then
+            if token then
+                -- expected the end
+                return unpack(non_greedy)
+            end
+            next_matcher = true
+
+        elseif mod == '^' then
+            if token_index ~= 1 then
+                -- expected the start
+                return unpack(non_greedy)
+            end
+            next_matcher = true
+
+        elseif not token then
+            -- ran out of tokens before the end of the seq
+            return unpack(non_greedy)
+        else
+
+            local matches = apply_matcher(matcher, token, str)
+            if matches then
+                wish.table.extend(values, matches)
+                token_index = token_index + 1
+            end
+
+            if mod == '*' or mod == '*?' then
+                next_matcher = not matches
+            elseif mod == '+' or mod == '+?' then
+                mod = '*' .. string.sub(mod, 2)
+                next_matcher = not matches
+            elseif matches then
+                next_matcher = true
+            else
+                -- no match
+                return unpack(non_greedy)
+            end
+        end
+
+        if next_matcher then
+            seq_index = seq_index + 1
+            matcher = seq[seq_index]
+            mod = matcher and matcher.mod
+        end
+    end
+    return token_index, values
+end
+
+function M.apply_seq(seq, tokens, str, callback)
+    local matched = false
+    local token_index = 1
+    while token_index <= #tokens do
+        local finish, values = apply_seq_at(seq, 1, tokens, str, token_index)
+        if finish then
+            matched = true
+            token_index = finish
+            callback(values)
+        else
+            token_index = token_index + 1
+        end
+    end
+    return matched
+end
+
+function M.apply_rules(rules, tokens, str, callback)
+    for i = 1, #rules do
+        M.apply_seq(rules[i], tokens, str, callback)
+    end
+    for i = 1, #tokens do
+        if tokens[i].nested then
+            M.apply_rules(rules, tokens[i].nested, str, callback)
+        end
+    end
+end
+
+local CALLBACKS = {}
+
+function M.add_buffer_callback(func)
+    table.insert(CALLBACKS, func)
+end
+
+local prev_buffer = nil
+local prev_complete = false
+local prev_tokens = nil
+
+local function parse_buffer()
+    local buffer = wish.get_buffer()
+    -- rehighlight if last buffer was not a valid zsh command
+    -- or the new buffer has changed (excepting ending whitespace changes)
+    if not prev_complete or string.sub(buffer, 1, #prev_buffer) ~= prev_buffer or string.find(buffer, '%S', #prev_buffer+1) then
+        -- is this going to be slow? do we need a debounce or something?
+        prev_complete, prev_tokens = wish.parse(buffer)
+        wish.log.debug(wish.repr(M.debug_tokens(prev_tokens, buffer), true))
+        prev_buffer = buffer
+        return true
+    end
+end
+
+function M.parse_buffer()
+    parse_buffer()
+    return prev_tokens, prev_buffer
+end
+
+wish.add_event_callback('buffer_change', function()
+    -- don't bother if no-one cares about the syntax tree
+    if #CALLBACKS == 0 or not parse_buffer() then
+        return
+    end
+
+    local shift = 0
+    for i = 1, #CALLBACKS do
+        if CALLBACKS[i](prev_tokens, prev_buffer) then
+            -- remove this callback
+            shift = shift + 1
+            CALLBACKS[i] = nil
+        elseif shift > 0 then
+            CALLBACKS[i - shift] = CALLBACKS[i]
+            CALLBACKS[i] = nil
+        end
+    end
+end)
+
+return M
