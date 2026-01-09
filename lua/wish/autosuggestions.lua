89e2d0de1daff6d7d5db351cea11d19df199eb1e
林千里 <lincheney@gmail.com>
2026-01-10 00:52:34 +1100

    cache history

--- lua/wish/autosuggestions.lua
+++ lua/wish/autosuggestions.lua
@@ -2,6 +2,11 @@ local M = {}
 
 local NAMESPACE = wish.add_buf_highlight_namespace()
 local suggestion = ''
+local history = nil
+
+wish.add_event_callback('accept_line', function()
+    history = nil
+end)
 
 wish.add_event_callback('buffer_change', function()
     local buffer = wish.get_buffer()
@@ -11,8 +16,9 @@ wish.add_event_callback('buffer_change', function()
         suggestion = nil
     elseif not suggestion or suggestion:sub(1, #buffer) ~= buffer then
         suggestion = nil
+        -- refetch history
+        history = history or ({wish.get_history()})[2]
         -- find a new one
-        local current, history = wish.get_history()
         for i = 1, #history do
             if history[i].text:sub(1, #buffer) == buffer then
                 -- got one
