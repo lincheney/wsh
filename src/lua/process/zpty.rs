24ef72c1abecfd9bbc14ae941d2fff1599c386e8
林千里 <lincheney@gmail.com>
2026-01-28 21:08:01 +1100

    tokio::fs::File probably assumes a disk file, so its broken

--- src/lua/process/zpty.rs
+++ src/lua/process/zpty.rs
@@ -3,7 +3,10 @@ use std::time::SystemTime;
 use std::default::Default;
 use anyhow::{Result};
 use mlua::{prelude::*};
-use tokio::io::{BufStream};
+use std::io::{Read, Write};
+use std::pin::Pin;
+use std::task::{ready, Context, Poll};
+use tokio::io::{BufStream, unix::AsyncFd, ReadBuf, AsyncRead, AsyncWrite};
 use tokio::sync::{watch};
 use serde::{Deserialize};
 use crate::ui::{Ui};
@@ -26,6 +29,53 @@ enum ZptyArgs {
     Full(FullZptyArgs),
 }
 
+struct AsyncZpty {
+    inner: AsyncFd<std::fs::File>,
+}
+
+impl std::os::fd::AsRawFd for AsyncZpty {
+    fn as_raw_fd(&self) -> std::os::fd::RawFd {
+        self.inner.as_raw_fd()
+    }
+}
+
+impl AsyncRead for AsyncZpty {
+    fn poll_read(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut ReadBuf<'_>) -> Poll<std::io::Result<()>> {
+        loop {
+            let mut guard = ready!(self.inner.poll_read_ready(cx))?;
+            let unfilled = buf.initialize_unfilled();
+            match guard.try_io(|inner| inner.get_ref().read(unfilled)) {
+                Ok(Ok(len)) => {
+                    buf.advance(len);
+                    return Poll::Ready(Ok(()));
+                },
+                Ok(Err(err)) => return Poll::Ready(Err(err)),
+                Err(_would_block) => continue,
+            }
+        }
+    }
+}
+
+impl AsyncWrite for AsyncZpty {
+    fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<std::io::Result<usize>> {
+        loop {
+            let mut guard = ready!(self.inner.poll_write_ready(cx))?;
+            match guard.try_io(|inner| inner.get_ref().write(buf)) {
+                Ok(result) => return Poll::Ready(result),
+                Err(_would_block) => continue,
+            }
+        }
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+}
+
 pub async fn zpty(ui: Ui, lua: Lua, val: LuaValue) -> Result<LuaMultiValue> {
     let args = match lua.from_value(val)? {
         ZptyArgs::Full(args) => args,
@@ -46,7 +96,8 @@ pub async fn zpty(ui: Ui, lua: Lua, val: LuaValue) -> Result<LuaMultiValue> {
     // do not drop the pty fd as zsh will do it for us
     // so we dup the fd to one we own instead
     let pty = crate::utils::dup_fd(unsafe{ std::os::fd::BorrowedFd::borrow_raw(zpty.fd) })?;
-    let pty = tokio::fs::File::from_std(pty.into());
+    // crate::utils::set_nonblocking_fd(&pty)?;
+    let pty = AsyncZpty{ inner: AsyncFd::new(pty.into())? };
     let pty = ReadWriteFile{
         inner: Some(BufStream::new(pty)),
         is_tty_master: true,
