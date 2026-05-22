7daf469349bebaeea9cac8e410c1cd2e5aa66836
林千里 <lincheney@gmail.com>
2026-05-22 23:54:07 +1000

    source from ~/.config/wish

--- lua/wish.lua
+++ lua/wish.lua
@@ -1,13 +1,28 @@
-require('wish/api')
-wish.iter = require('wish/iter')
-wish.table = require('wish/table')
-wish.style = require('wish/style')
-wish.utf8 = require('wish/utf8')
-require('wish/keybind')
-require('wish/syntax-highlight')
-require('wish/live-preview')
-require('wish/autosuggestions')
-require('wish/status_bar')
-require('wish/rainbow-brackets')
+require('wish.api')
+wish.plugin = require('wish.plugin')
+wish.iter = require('wish.iter')
+wish.table = require('wish.table')
+wish.style = require('wish.style')
+wish.utf8 = require('wish.utf8')
+require('wish.keybind')
+
+require('wish.syntax-highlight').enable()
+require('wish.extras.rainbow-brackets').enable()
+
+local config_home = os.getenv('XDG_CONFIG_HOME')
+if not config_home then
+    local home = os.getenv('HOME')
+    config_home = home and home .. '/.config/wish/'
+end
+if config_home then
+    package.path = config_home .. '?.lua;' .. package.path
+    local file = io.open(config_home .. 'init.lua', "r")
+    if file then
+        file:close()
+        dofile(config_home .. 'init.lua')
+    end
+end
+
+-- require('wish.extras.mouse-demo').enable()
 
 return wish
