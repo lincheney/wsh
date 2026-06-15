a1e121b80a8f46074e96f8fa771d2e8cbe015cc4
林千里 <lincheney@gmail.com>
2026-06-15 19:30:50 +1000

    do i need this?

--- build.rs
+++ build.rs
@@ -21,6 +21,9 @@ fn main() {
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
+    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-changed=zsh.h");
+
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
