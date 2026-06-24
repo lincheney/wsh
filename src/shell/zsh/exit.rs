9c37525aeab540c6404ad8a0dd9f09bba58702a5
林千里 <lincheney@gmail.com>
2026-06-25 05:50:17 +1000

    expose exit_val to lua

--- src/shell/zsh/exit.rs
+++ src/shell/zsh/exit.rs
@@ -3,10 +3,11 @@ use crate::shell::externs::GlobalState;
 use crate::lua::HasEventCallbacks;
 
 pub extern "C" fn exit_hook(_hook: zsh_sys::Hookdef, _arg: *mut c_void) -> c_int {
+    let exit_val = unsafe{ zsh_sys::exit_val };
     let _ = GlobalState::with(|ui| {
         let ui = ui.clone();
         crate::log_if_err(ui.clone().shell_loop(false, async move {
-            crate::log_if_err(ui.trigger_exit_callbacks().await);
+            crate::log_if_err(ui.trigger_exit_callbacks(exit_val).await);
         }));
     });
     crate::shell::externs::teardown();
@@ -22,7 +23,7 @@ pub fn init() {
 pub fn cleanup() {
     unsafe {
         // do NOT deletehookfunc while in the middle of exiting, will cause use after free
-        if zsh_sys::exit_pending == 0 {
+        if zsh_sys::shell_exiting == 0 {
             zsh_sys::deletehookfunc(c"exit".as_ptr().cast_mut(), Some(exit_hook));
         }
     }
