d933ac65436558ec8580ea7b26f754ad3902b363
林千里 <lincheney@gmail.com>
2026-06-29 18:48:04 +1000

    thread local is not signal safe

--- src/shell/zsh/signals/sigchld.rs
+++ src/shell/zsh/signals/sigchld.rs
@@ -1,6 +1,6 @@
 use std::ptr::null_mut;
 use bstr::{BString};
-use std::cell::{RefCell};
+use std::sync::Mutex;
 use tokio::sync::{oneshot};
 use std::collections::HashMap;
 use anyhow::Result;
@@ -20,18 +20,21 @@ enum Output {
 }
 
 struct State {
-    sink: Option<Sink>,
+    sink: Option<Option<Sink>>,
     pids: ConstHashMap<Pid, bool>,
     output: VecDeque<Output>,
 }
 
-thread_local! {
-    static STATE: SignalSafeWrapper<RefCell<State>> = SignalSafeWrapper::new(RefCell::new(State {
-        sink: Sink::new().ok(),
+unsafe impl Send for State {}
+unsafe impl Sync for State {}
+
+static STATE: SignalSafeWrapper<Mutex<State>> = const {
+    SignalSafeWrapper::new(Mutex::new(State {
+        sink: None,
         pids: ConstHashMap::new(),
         output: VecDeque::new(),
-    }));
-}
+    }))
+};
 
 fn jobtab_retain_iter<'a, F: FnMut(&'a zsh_sys::process) -> bool>(_token: &QueuedSignalToken, mut callback: F) -> impl Iterator<Item=&'a zsh_sys::process> {
     unsafe {
@@ -69,27 +72,21 @@ fn jobtab_iter<'a>(token: &QueuedSignalToken) -> impl Iterator<Item=&'a zsh_sys:
 }
 
 pub fn clear_pids() {
-    STATE.with(|state| {
-        state.get().borrow_mut().pids.clear();
-    });
+    STATE.get().lock().unwrap().pids.clear();
 }
 
 pub fn register_pid(ui: &crate::ui::Ui, pid: Pid, add_to_jobtab: bool) -> Result<oneshot::Receiver<i32>> {
     let (sender, receiver) = oneshot::channel();
     ui.try_borrow_mut()?.pid_map.insert(pid, sender);
-    STATE.with(|state| {
-        state.get().borrow_mut().pids.insert(pid, add_to_jobtab);
-    });
+    STATE.get().lock().unwrap().pids.insert(pid, add_to_jobtab);
     Ok(receiver)
 }
 
 pub fn deregister_pid(ui: &crate::ui::Ui, pid: Pid) -> Result<()> {
-    STATE.with(|state| {
-        let _ = super::with_queued_signals(|token| {
-            if matches!(state.get_with_token(token).borrow_mut().pids.remove(&pid), Some(true)) {
-                jobtab_retain_iter(token, |proc| proc.pid != pid).for_each(|_| ());
-            }
-        });
+    let _ = super::with_queued_signals(|token| {
+        if matches!(STATE.get_with_token(token).lock().unwrap().pids.remove(&pid), Some(true)) {
+            jobtab_retain_iter(token, |proc| proc.pid != pid).for_each(|_| ());
+        }
     });
     ui.try_borrow_mut()?.pid_map.remove(&pid);
     Ok(())
@@ -115,58 +112,57 @@ pub(super) fn sighandler(trapped: bool) -> c_int {
         let _ = super::with_queued_signals(|token| {
             let mut notify = false;
 
-            STATE.with(|state| {
-                let state = &mut *state.get_with_token(token).borrow_mut();
+            let state = &mut *STATE.get_with_token(token).lock().unwrap();
 
-                // register any pids we are interested in
-                for (&pid, &add) in state.pids.iter() {
-                    // register these pids
-                    if add && !jobtab_iter(token).any(|proc| proc.pid == pid) {
-                        crate::shell::zsh::add_pid(pid);
-                    }
+            // register any pids we are interested in
+            for (&pid, &add) in state.pids.iter() {
+                // register these pids
+                if add && !jobtab_iter(token).any(|proc| proc.pid == pid) {
+                    crate::shell::zsh::add_pid(pid);
                 }
+            }
 
-                let guard = state.sink.as_mut().map(|sink| {
-                    sink.clear();
-                    sink.override_shout(false, true)
-                });
-                // replace zle_entry_ptr to avoid deadlocks
-                let old_zle_entry_ptr = zsh_sys::zle_entry_ptr;
-                zsh_sys::zle_entry_ptr = Some(sigchld_zle_entry_ptr_override);
-                // reset thisjob so it doesn't go off reporting job statuses for foreground jobs
-                // call wait_for_processes instead of zhandler
-                // zhandler only calls wait_for_processes and no traps anyway
-                // but importantly we need to bypass queueing
-                if trapped {
-                    let thisjob = zsh_sys::thisjob;
-                    zsh_sys::thisjob = THIS_JOB;
-                    zsh_sys::wait_for_processes();
-                    zsh_sys::thisjob = thisjob;
-                } else {
-                    zsh_sys::wait_for_processes();
-                }
-                zsh_sys::zle_entry_ptr = old_zle_entry_ptr;
-                drop(guard);
-                let output = state.sink.as_mut().and_then(|sink| sink.read());
-
-                if let Some(output) = output {
-                    notify = true;
-                    state.output.push_back(Output::Shout(output));
-                }
-
-                jobtab_retain_iter(token, |proc| {
-                    // found one
-                    if proc.status >= 0 && let Some(added) = state.pids.remove(&proc.pid) {
-                        notify = true;
-                        state.output.push_back(Output::Status{pid: proc.pid, status: proc.status});
-                        // pop it off
-                        if added {
-                            return false
-                        }
-                    }
-                    true
-                }).for_each(drop);
+            let sink = state.sink.get_or_insert_with(|| Sink::new().ok());
+            let guard = sink.as_mut().map(|sink| {
+                sink.clear();
+                sink.override_shout(false, true)
             });
+            // replace zle_entry_ptr to avoid deadlocks
+            let old_zle_entry_ptr = zsh_sys::zle_entry_ptr;
+            zsh_sys::zle_entry_ptr = Some(sigchld_zle_entry_ptr_override);
+            // reset thisjob so it doesn't go off reporting job statuses for foreground jobs
+            // call wait_for_processes instead of zhandler
+            // zhandler only calls wait_for_processes and no traps anyway
+            // but importantly we need to bypass queueing
+            if trapped {
+                let thisjob = zsh_sys::thisjob;
+                zsh_sys::thisjob = THIS_JOB;
+                zsh_sys::wait_for_processes();
+                zsh_sys::thisjob = thisjob;
+            } else {
+                zsh_sys::wait_for_processes();
+            }
+            zsh_sys::zle_entry_ptr = old_zle_entry_ptr;
+            drop(guard);
+            let output = sink.as_mut().and_then(|sink| sink.read());
+
+            if let Some(output) = output {
+                notify = true;
+                state.output.push_back(Output::Shout(output));
+            }
+
+            jobtab_retain_iter(token, |proc| {
+                // found one
+                if proc.status >= 0 && let Some(added) = state.pids.remove(&proc.pid) {
+                    notify = true;
+                    state.output.push_back(Output::Status{pid: proc.pid, status: proc.status});
+                    // pop it off
+                    if added {
+                        return false
+                    }
+                }
+                true
+            }).for_each(drop);
 
             // notify that we found something
             if notify {
@@ -181,18 +177,16 @@ pub(super) fn sighandler(trapped: bool) -> c_int {
 
 pub fn handle_sigchld(ui: &crate::ui::Ui) -> Result<()> {
     let pid_map = &mut ui.try_borrow_mut()?.pid_map;
-    STATE.with(|state| {
-        for x in state.get().borrow_mut().output.drain(..) {
-            match x {
-                Output::Status{pid, status} => if let Some(sender) = pid_map.remove(&pid) {
-                    let _ = sender.send(status);
-                },
-                Output::Shout(output) => {
-                    ui.handle_sigchld_shout(output);
-                },
-            }
+    for x in STATE.get().lock().unwrap().output.drain(..) {
+        match x {
+            Output::Status{pid, status} => if let Some(sender) = pid_map.remove(&pid) {
+                let _ = sender.send(status);
+            },
+            Output::Shout(output) => {
+                ui.handle_sigchld_shout(output);
+            },
         }
-    });
+    }
     Ok(())
 }
 
@@ -209,11 +203,9 @@ pub(super) fn cleanup() {
     unsafe {
         zsh_sys::deletehookfunc(c"before_trap".as_ptr().cast_mut(), Some(before_trap_hook));
     }
-    STATE.with(|state| {
-        let mut state = state.get().borrow_mut();
-        state.pids.clear();
-        state.output.clear();
-    });
+    let mut state = STATE.get().lock().unwrap();
+    state.pids.clear();
+    state.output.clear();
 }
 
 pub(super) fn init(ui: &crate::ui::Ui) -> crate::ui::Ui {
